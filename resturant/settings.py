035a388ffe0923234bae3f5c289e91adbaaa2135
Kosar Hoseini <Kosarrrrr.78@gmail.com>
2024-01-29 21:37:02 -0800

    add consistency in setting

--- resturant/settings.py
+++ resturant/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main', 'static'),
 ]
 
+#login redirect
+LOGIN_REDIRECT_URL = 'food_list'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -131,5 +134,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
 
